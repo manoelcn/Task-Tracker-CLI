f7e12fe8eee1c94f9e2de893650e10dc5efbf685
Manoel <candidomanoel78@gmail.com>
2026-02-26 18:55:30 -0300

    feat: cria função para carregar tarefas

---
+++ storage.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+
+def load_tasks():
+    if not os.path.exists('tasks.json'):
+        with open("tasks.json", "w") as f:
+            json.dump([], f)
+        return []
+    with open("tasks.json", "r") as f:
+        return json.load(f)
